6cf3c34fb3edce7d93746f59c9e9eb5d5922d7e0
Laceinstein <laceinstein@gmail.com>
2022-09-15 18:00:31 +0200

    add build

---
+++ build/precache-manifest.78470ff710f417dbfcda3d9a9992a1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cbaa840e1c7561a9310f9ab47a2052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acf30bb69660903c75e6",
+    "url": "/static/css/main.108ab02e.chunk.css"
+  },
+  {
+    "revision": "639f69c36b3216d85e6a",
+    "url": "/static/js/2.2870161e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2870161e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf30bb69660903c75e6",
+    "url": "/static/js/main.326d18b8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
